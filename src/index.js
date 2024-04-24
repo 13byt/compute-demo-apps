7af2a03722ec4003f82fc5e81c8e821677889f63
rupesh-iagon <rupesh@ngit.com.np>
2024-04-24 14:36:56 +0545

    Add message by ready from env

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! (by ${process.env.MESSAGE_BY}) `)
 })
 
 app.listen(process.env.IAGON_COMPUTE_PORT, () => {
